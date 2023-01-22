587cfae638f37504f10b6ee63399b7c5ffd042fc
Oleksii-Kramar <dp020991kos@gmail.com>
2023-01-22 23:39:01 +0100

    first-react-app

---
+++ src/components/app/app.js
@@ -0,0 +1,33 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployersList from '../employers-list/employers-list';
+import EmployersAddForm from '../employers-add-form/employers-add-form';
+
+import './app.css';
+
+function App() {
+
+const data = [
+    {name: 'Jonh C.', salary: 800, increase: false},
+    {name: 'Alex K.', salary: 3000, increase: true},
+    {name: 'Max B.', salary: 5000, increase: false},
+]
+
+    return (
+        <div className="app">
+            <AppInfo/>
+
+            <div className="search-panel">
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+
+            <EmployersList data={data}/>
+            <EmployersAddForm/>
+        </div>
+    );
+}
+
+
+export default App;
