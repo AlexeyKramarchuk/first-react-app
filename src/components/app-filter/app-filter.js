741c679f68741bc44a27af7856b58c89c75cf5fa
Oleksii-Kramar <dp020991kos@gmail.com>
2023-01-27 21:06:59 +0100

    filter_commit

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,9 +1,31 @@
 import './app-filter.css';
 
-const AppFilter = () => {
+const AppFilter = (props) => {
+const buttonsData = [
+    {name: 'all', label: 'All employees'},
+    {name: 'rise', label: 'On the rise'},
+    {name: 'moreThen1000', label: 'Salary is more than $1000'},
+];
+
+const buttons = buttonsData.map(({name, label}) => {
+    const active = props.filter === name;
+    const clazz = active ? 'btn-light' : 'btn-outline-light';
+    return (
+        <button 
+            className={`btn ${clazz}`}
+            type="button"
+            key={name}
+            onClick={() => props.onFilterSelect(name)}>
+            {label}
+        </button>
+    )
+})
+
+
     return (
         <div className="btn-group">
-            <button 
+            {buttons}
+            {/* <button 
                 className="btn btn-light"
                 type="button">
                     All employees
@@ -16,8 +38,8 @@ const AppFilter = () => {
             <button 
                 className="btn btn-outline-light"
                 type="button">
-                    Salary is more than $1000.
-            </button>
+                    Salary is more than $1000
+            </button> */}
         </div>
     )
 }
